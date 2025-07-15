0744531bf4b951c4fa6abbf46348a1160d8ded2f
Rebecca Machio <ajiamborebecca26@gmail.com>
2025-07-15 12:10:06 +0300

    Implementing Models

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import title, author, publication_year
 
-# Register your models here.
+class Book (admin.ModelAdmin):
+
+    list_display = ('title', 'author', 'publication_year')
+    search_fields = ('title', 'author')
