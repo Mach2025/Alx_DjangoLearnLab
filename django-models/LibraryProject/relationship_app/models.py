f310abad4fc9c3552d8ed8421d49ca284309c21c
Rebecca Machio <ajiamborebecca26@gmail.com>
2025-07-17 14:05:29 +0300

    Django models changes

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,6 +1,35 @@
-from django.db import models
+# from django.db import models
 
-# Create your models here.
+# # Create your models here.
+# from django.db import models
+
+# class Author(models.Model):
+#     name = models.CharField(max_length=100)
+
+#     def __str__(self):
+#         return self.name
+
+# class Book(models.Model):
+#     title = models.CharField(max_length=200)
+#     author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+#     def __str__(self):
+#         return self.title
+
+# class Library(models.Model):
+#     name = models.CharField(max_length=100)
+#     books = models.ManyToManyField(Book)
+
+#     def __str__(self):
+#         return self.name
+
+# class Librarian(models.Model):
+#     name = models.CharField(max_length=100)
+#     library = models.OneToOneField(Library, on_delete=models.CASCADE)
+
+#     def __str__(self):
+#         return self.name
+
 from django.db import models
 
 class Author(models.Model):
@@ -9,24 +38,17 @@ class Author(models.Model):
     def __str__(self):
         return self.name
 
+class Library(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
 class Book(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=100)
+    publication_year = models.PositiveIntegerField()
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    library = models.ForeignKey(Library, on_delete=models.CASCADE, related_name='books')
 
     def __str__(self):
         return self.title
-
-class Library(models.Model):
-    name = models.CharField(max_length=100)
-    books = models.ManyToManyField(Book)
-
-    def __str__(self):
-        return self.name
-
-class Librarian(models.Model):
-    name = models.CharField(max_length=100)
-    library = models.OneToOneField(Library, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.name
-
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,21 +1,35 @@
-from django.shortcuts import render
-from .models import Book
+# from django.shortcuts import render
+# from .models import Book
 
+# def list_books(request):
+#     books = Book.objects.select_related('author').all()
+#     return render(request, 'list_books.html', {'books': books})
+
+# from django.views.generic.detail import DetailView
+# from .models import Library
+
+# class LibraryDetailView(DetailView):
+#     model = Library
+#     template_name = 'library_detail.html'
+#     context_object_name = 'library'
+
+# from django.shortcuts import render
+# from .models import Book
+
+# def list_books(request):
+#     books = Book.objects.all()
+#     return render(request, 'list_books.html', {'books': books})
+from django.shortcuts import render
+from django.views.generic.detail import DetailView
+from .models import Book, Library
+
+# Function-based view
 def list_books(request):
-    books = Book.objects.select_related('author').all()
+    books = Book.objects.all()
     return render(request, 'list_books.html', {'books': books})
 
-from django.views.generic.detail import DetailView
-from .models import Library
-
+# Class-based view
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'library_detail.html'
     context_object_name = 'library'
-
-from django.shortcuts import render
-from .models import Book
-
-def list_books(request):
-    books = Book.objects.all()
-    return render(request, 'list_books.html', {'books': books})
