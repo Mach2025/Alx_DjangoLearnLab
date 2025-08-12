ece315c50b6629dd08b7a7d5910baf81c30c3df8
Rebecca Machio <ajiamborebecca26@gmail.com>
2025-08-12 16:48:58 +0300

    Implementing Advanced Features: Tagging and Search Functionality

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from taggit.managers import TaggableManager
+from taggit.models import Tag
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
