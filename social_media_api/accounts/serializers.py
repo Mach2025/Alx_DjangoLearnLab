9a4e0d9b25eeed4c55306b8798cacaef5cb28c41
Rebecca Machio <ajiamborebecca26@gmail.com>
2025-08-19 12:04:43 +0300

    Social Media Api SetUp

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -7,14 +7,6 @@ class UserSerializer(serializers.ModelSerializer):
         model = CustomUser
         fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'followers']
 
-
-class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
-
-    class Meta:
-        model = CustomUser
-        fields = ['id', 'username', 'email', 'password']
-
     def create(self, validated_data):
         user = CustomUser.objects.create_user(
             username=validated_data['username'],
@@ -24,12 +16,4 @@ class RegisterSerializer(serializers.ModelSerializer):
         return user
 
 
-class LoginSerializer(serializers.Serializer):
-    username = serializers.CharField()
-    password = serializers.CharField()
 
-    def validate(self, data):
-        user = authenticate(username=data['username'], password=data['password'])
-        if not user:
-            raise serializers.ValidationError("Invalid credentials")
-        return user
