806a2ddcaaeb7ee9410323f0feb3e78ead2d184f
Rebecca Machio <ajiamborebecca26@gmail.com>
2025-08-19 13:31:53 +0300

    Followers and Unfollowers Feed

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -6,7 +6,7 @@ router = DefaultRouter()
 router.register(r"posts", PostViewSet, basename="post")
 router.register(r"comments", CommentViewSet, basename="comment")
 
-urlpatterns = router.urls
+#urlpatterns = router.urls
 
 urlpatterns = [
     path("", include(router.urls)),
